26df73d8d6aa6ced4062074f9da715b0942b5268
Tim Sanderson <twsand11@gmail.com>
2019-12-02 09:41:48 -0700

    Refactored for cleanliness, changed db from absolute to relative path

--- server/index.js
+++ server/index.js
@@ -3,10 +3,9 @@ const app = express();
 const path = require('path')
 const port = 8091;
 const indexPATH = path.join(__dirname, '..', 'public', 'dist')
-const db = require('/Users/tim/Desktop/featureReviews/db/index.js')
+const db = require('../db/index.js')
 
 
-console.log(db)
 app.use(express.json());
 
 
