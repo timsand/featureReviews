e7ac2790db10a5b11c2e9256ba551e17d8b4de0c
Tim Sanderson <twsand11@gmail.com>
2019-12-02 21:01:58 -0700

    Added avatar, minor CSS

--- client/src/comment.jsx
+++ client/src/comment.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import Rating from '@material-ui/lab/Rating';
+import Avatar from '@material-ui/core/Avatar';
 import BottomButtons from './BottomButtons.jsx';
 
 
 const Comment = (props) => {
   return (
     <div className='commentBox'>
-      <h5>{props.person}</h5>
-      <h4>{props.title}</h4>
-      <Rating name="testRating" value={props.rating} readOnly={true} size="small"/>
+      <div className="tsAvatarContainer">
+        <Avatar>?</Avatar>
+        <h5>{props.person}</h5>
+      </div>
+      <div className="tsReviewTitleContainer">
+        <Rating name="testRating" value={props.rating} readOnly={true} size="small" />
+        <h4>{props.title}</h4>
+      </div>
       <h5>{props.date}</h5>
-      <h5>VERIFIED PURCHASE</h5>
+      <h5 className="tsVerifiedPurchase">Verified Purchase</h5>
       <p>{props.body}</p>
       <span>{props.helpfulCount} people found this helpful</span>
-      <BottomButtons id={props.id} helpfulClicked={props.helpfulClicked} buttonClicked={props.buttonClicked}/>
+      <BottomButtons id={props.id} helpfulClicked={props.helpfulClicked} buttonClicked={props.buttonClicked} />
     </div>
   )
 }
