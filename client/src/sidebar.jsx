5ad24ebd0bbf9ec4de75251f14252b00d4229aec
Tim Sanderson <twsand11@gmail.com>
2019-12-04 14:35:34 -0600

    Histogram title CSS

--- client/src/sidebar.jsx
+++ client/src/sidebar.jsx
@@ -9,45 +9,49 @@ const Sidebar = (props) => {
   const threeStarRatings = props.individualRatings[0] === undefined ? 0 : (Math.floor((props.individualRatings[2].threeStarRatings / props.totalComments) * 100));
   const fourStarRatings = props.individualRatings[0] === undefined ? 0 : (Math.floor((props.individualRatings[3].fourStarRatings / props.totalComments) * 100));
   const fiveStarRatings = props.individualRatings[0] === undefined ? 0 : (Math.floor((props.individualRatings[4].fiveStarRatings / props.totalComments) * 100));
+  const totalRating = Math.round(props.totalRating * 10) / 10;
   // oneStarRatings = oneStarRatings * 100;
   return (
     <div>
       <div>
-        <h4>Customer reviews</h4>
-        <Rating name="sidebarStars" value={props.totalRating} readOnly={true} precision={0.1} size="medium"/>
-        <span>{props.totalRatings}</span>
+        <h4 id="tsCustomerTitle">Customer reviews</h4>
+        <div className="tsHistogramTitle">
+          <Rating name="sidebarStars" value={totalRating} readOnly={true} precision={0.1} size="medium" />
+          <span class="tsHistogramRatingOverview">{totalRating} out of 5</span>
+        </div>
+        <span id="tsHistogramRatingCount">{props.totalComments} customer ratings</span>
         <div className="tsHistogramContainer" title={`5 stars representing ${fiveStarRatings}% of rating`}>
           <a class="tsHistogramStars">5 star</a>
           <div className="tsHistogramBarContainer">
-            <div className="tsHistogramBar" style={{width: `${fiveStarRatings}%`}}></div>
+            <div className="tsHistogramBar" style={{ width: `${fiveStarRatings}%` }}></div>
           </div>
           <a className="tsHistogramPercents">{fiveStarRatings}%</a>
         </div>
         <div className="tsHistogramContainer" title={`4 stars representing ${fourStarRatings}% of rating`}>
           <a class="tsHistogramStars">4 star</a>
           <div className="tsHistogramBarContainer">
-            <div className="tsHistogramBar" style={{width: `${fourStarRatings}%`}}></div>
+            <div className="tsHistogramBar" style={{ width: `${fourStarRatings}%` }}></div>
           </div>
           <a className="tsHistogramPercents">{fourStarRatings}%</a>
         </div>
         <div className="tsHistogramContainer" title={`3 stars representing ${threeStarRatings}% of rating`}>
           <a class="tsHistogramStars">3 star</a>
           <div className="tsHistogramBarContainer">
-            <div className="tsHistogramBar" style={{width: `${threeStarRatings}%`}}></div>
+            <div className="tsHistogramBar" style={{ width: `${threeStarRatings}%` }}></div>
           </div>
           <a className="tsHistogramPercents">{threeStarRatings}%</a>
         </div>
         <div className="tsHistogramContainer" title={`2 stars representing ${twoStarRatings}% of rating`}>
           <a class="tsHistogramStars">2 star</a>
           <div className="tsHistogramBarContainer">
-            <div className="tsHistogramBar" style={{width: `${twoStarRatings}%`}}></div>
+            <div className="tsHistogramBar" style={{ width: `${twoStarRatings}%` }}></div>
           </div>
           <a className="tsHistogramPercents">{twoStarRatings}%</a>
         </div>
         <div className="tsHistogramContainer" title={`1 stars representing ${oneStarRatings}% of rating`}>
           <a class="tsHistogramStars">1 star</a>
           <div className="tsHistogramBarContainer">
-            <div className="tsHistogramBar" style={{width: `${oneStarRatings}%`}}></div>
+            <div className="tsHistogramBar" style={{ width: `${oneStarRatings}%` }}></div>
           </div>
           <a className="tsHistogramPercents">{oneStarRatings}%</a>
         </div>
