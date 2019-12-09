2f596459305d1918c8ca2998d94fb83e9c46b457
Tim Sanderson <twsand11@gmail.com>
2019-12-09 11:41:50 -0600

    Added filtering and styling for more than 10 comments

--- client/src/commentContainer.jsx
+++ client/src/commentContainer.jsx
@@ -4,12 +4,13 @@ import { Hidden } from '@material-ui/core';
 
 
 const CommentContainer = (props) => {
-  var comments = props.comments.map((val, i) => {
+  let comments = props.comments.map((val, i) => {
     return <Comment 
     id={val.id} body={val.body} date={val.date} helpfulCount={val.helpfulCount} itemName={val.itemName} person={val.person[0]} rating={val.rating} 
     title={val.title} key={i} helpfulClicked={props.helpfulClicked} buttonClicked={val.buttonClicked} avatarUrl={val.person[1]} verified={val.verified}>
     </Comment>
   })
+  let renderedComments = comments.splice(0, props.commentNumberToDisplay);
   return (
     <div>
       <span>{props.comments.length} customer reviews</span>
@@ -20,11 +21,11 @@ const CommentContainer = (props) => {
       <div>
         <a id="tsClearFilter" onClick={props.clearFilter} style={props.clearFilter ? {visibility: 'visible'} : {visibility: 'hidden'}}>Clear filter</a>
       </div>
-      {comments}
+      {renderedComments}
       <div className="tsBottomCommentBox">
         There is {props.comments.length} customer review and {props.numberOfRatings} customer ratings.
       </div>
-      <a>See all customer reviews</a>
+      <a id="tsShowAllReviews" onClick={props.showAllReviews}>See all customer reviews</a>
       <button className="commentBoxSubmitReview">Write a customer review</button>
     </div>
   )
