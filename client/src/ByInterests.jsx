2f161683a9ed8a9e489ef54ece53170bc246f59c
Tim Sanderson <twsand11@gmail.com>
2019-12-10 19:28:55 -0600

    Hardcoded byInterests

---
+++ client/src/ByInterests.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Rating from '@material-ui/lab/Rating'
+
+
+const ByInterests = (props) => {
+
+
+
+  return (
+    <div>
+      <h3>By customer groups and interests</h3>
+      <div>
+        <span>Survival</span>
+        <Rating name="survival" precision={0.2} value={4.5} readOnly={true} size="small"/>
+        <span>4.5</span>
+      </div>
+      <div>
+        <span>Scavenging</span>
+        <Rating name="scavenging" precision={0.2} value={4.3} readOnly={true} size="small"/>
+        <span>4.3</span>
+      </div>
+      <div>
+        <span>Is this feature helpful?</span>
+        <button>Yes</button>
+        <button>No</button>
+      </div>
+      <div className="tsSeperator"></div>
+    </div>
+  )
+}
+
+export default ByInterests;
--- client/src/sidebar.jsx
+++ client/src/sidebar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Rating from '@material-ui/lab/Rating';
 import ByFeature from './ByFeature.jsx';
+import ByInterests from './ByInterests.jsx';
 
 
 const Sidebar = (props) => {
@@ -57,6 +58,7 @@ const Sidebar = (props) => {
       </div>
       <div className="tsSeperator"></div>
       <ByFeature categoryRatings={props.categoryRatings}/>
+      <ByInterests />
       <div>
         <h5 id="tsReviewPromptTitle">Review this product</h5>
         <span style={{fontSize: '13px'}}>Share your thoughts with other customers</span>
