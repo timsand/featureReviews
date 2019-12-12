66077a74bc3fca06f90037b15c19913bccc86576
Tim Sanderson <twsand11@gmail.com>
2019-12-11 19:24:16 -0600

    Deleted commented out code

--- client/src/ByFeature.jsx
+++ client/src/ByFeature.jsx
@@ -5,12 +5,6 @@ import Rating from '@material-ui/lab/Rating'
 const ByFeature = (props) => {
   console.log(props);
   let mappedCategories;
-  // if (props.categoryRatings) {
-  //   let test = Object.keys(props.categoryRatings);
-  //   mappedCategories = props.categoryRatings.map((categories, i) => {
-  //     return <Rating value={categories.test[0].Overall} readOnly={true} precision={0.2} size="small"></Rating>
-  //   })
-  // }
 
   if (props.categoryRatings) {
     mappedCategories = Object.keys(props.categoryRatings).map(function(key, index) {
@@ -24,17 +18,6 @@ const ByFeature = (props) => {
     });
   }
 
-
-  // const mappedCategories = props.categoryRatings.map((category) => {
-  //   <div>
-  //     <Rating name={category.name}></Rating>
-  //   </div>
-  // })
-
-
-
-
-  // <Rating name="sidebarStars" value={totalRating} readOnly={true} precision={0.1} size="medium" />
   return (
     <div>
       <h3>By Feature</h3>
